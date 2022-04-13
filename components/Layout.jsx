13b0faf5bf1a3a91db0099efdac6eefb92a1d87a
TrinidadCarlos <carlostrinidad952@gmail.com>
2022-04-13 15:32:41 -0500

    modificacion de layout-se agrega Head y etiquetas meta, se elimina _document debido a que se movio el Head

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+
+import Head from 'next/head';
+
 import Footer from './Footer';
 import Header from './Header';
+
+const originSite = (typeof window === 'undefined') ? '' : window.location.origin;
 
 const Layout = ({children, showFooter}) => {
 
   return (
    <>
+       <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            property="og:title"
+            content="portafolio frontend"
+          />
+          <meta
+            property="og:description"
+            content="Portafolio frontEnd con NextJs"
+          />
+          <meta
+            property="og:image"
+            content={`${originSite}/images/sites/portafolio.png`}
+          />
+          <meta property="og:type" content="article" />
+        </Head>
+
          <Header />
          <main>
             {children}
